b4f5f8e6e083da7e2ed929e5985b95c774ea68f6
Adam Blum <adam@adamblum.digital>
2022-11-17 12:35:07 -0500

    Improve CreateModuleConsoleCommand input handling

--- src/Modules/Module/Application/Command/CreateModuleConsoleCommand.php
+++ src/Modules/Module/Application/Command/CreateModuleConsoleCommand.php
@@ -12,9 +12,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use App\Modules\Module\Application\Event\OnCreationRequestedEvent;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ *	@todo	Figure out how to use translations in Console Commands.
+ *			using `getenv('LANG')` gets an immediate result (en_US.UTF-8)
+ *			but there is likely a better way to do this.
+ */
 
 #[AsCommand(
-	name:			'app:create-module',
+	name:			'app:module:create',
 	description:	'Creates a new Module'
 )]
 final class CreateModuleConsoleCommand extends Command
@@ -35,23 +42,33 @@ final class CreateModuleConsoleCommand extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$helper = $this->getHelper('question');
+        $io = new SymfonyStyle($input, $output);
 
-		$output->writeln([
-			'Module Creator',
-			'--------------'
-		]);
+        $io->title('Module Creator');
 
-		$question = new Question('Enter the Module Title: ');
-		$title = $helper->ask($input, $output, $question);
+		$title = $io->ask('Enter the Module title', null, function($value) {
+			if (!is_string($value)) {
+				throw new \RuntimeException('The Module Title must be a string');
+			}
 
-		$output->writeln(['Creating Module']);
+			return $value;
+		});
 
+		/**
+		 *	Specifies mixed return type, though it always (in this 
+		 *	use case) returns string.
+		 *	@see Symfony\Component\Console\Style\SymfonyStyle:245	
+		 */
+		/** @phpstan-ignore-next-line */
+        if ($io->confirm(sprintf('Create Module with title %s?', $title), false)) {
 
-		$this->eventDispatcher->dispatch(new OnCreationRequestedEvent(
-			$title
-		));
-		
-		return Command::SUCCESS;
+			$io->success('Dispatching Creation Request');
+			/** @phpstan-ignore-next-line */
+			$this->eventDispatcher->dispatch(new OnCreationRequestedEvent($title));
+			return Command::SUCCESS;
+		} else {
+            $io->error('Module Creation aborted.');
+    		return Command::FAILURE;
+		}
 	}
 }
