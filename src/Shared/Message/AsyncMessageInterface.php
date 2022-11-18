7f9c01355bd918439c8444497d1e04cdfa9bb9b8
Adam Blum <adam@adamblum.digital>
2022-11-17 19:46:17 -0500

    Abstract async messages using a Shared Interface

--- src/Shared/Message/AsyncMessageInterface.php
+++ src/Shared/Message/AsyncMessageInterface.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 namespace App\Shared\Message;
-
+/**
+ *	This class is configured to use the 'async' transport
+ *	in `/config/packages/messenger.yaml`
+ *
+ *	Implement this interface in a message class if it 
+ *	should be send async
+ */
 interface AsyncMessageInterface
 {
 }
