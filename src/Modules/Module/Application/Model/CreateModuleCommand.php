bdb9bb40a19830a3de44c2a9a5f8e24807d81421
Adam Blum <adam@adamblum.digital>
2022-11-17 19:43:58 -0500

    Abstract async messages using a Shared Interface

--- src/Modules/Module/Application/Model/CreateModuleCommand.php
+++ src/Modules/Module/Application/Model/CreateModuleCommand.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\Modules\Module\Application\Model;
 
+use App\Shared\Message\AsyncMessageInterface;
+
 /**
- *	This message is handled via async transport
+ *	This message is handled via async transport.
+ *
+ *	config/packages/messenger.yaml
  */
-final class CreateModuleCommand
+final class CreateModuleCommand implements AsyncMessageInterface
 {
 	private string $title;
 
