db55d6e0e7dd993dee2b781433a8697c036d030b
Adam Blum <adam@adamblum.digital>
2022-11-27 19:34:43 -0500

    Modify StaticPage

--- src/Modules/StaticPage/Application/Controller/View/HomepageController.php
+++ src/Modules/StaticPage/Application/Controller/View/HomepageController.php
@@ -6,13 +6,11 @@ namespace App\Modules\StaticPage\Application\Controller\View;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Psr\Log\LoggerInterface;
 
 final class HomepageController extends AbstractController
 {
 	public function __invoke(): Response
 	{
-
 		return $this->render('@StaticPage/view/homepage.html.twig');
 	}
 }
